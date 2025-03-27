aa659f0846365a3685fa69e708d8230db3a4fca0
giovani <inf_giovani@hotmail.com>
2025-03-27 20:04:26 -0300

    Model com serealização

--- lib/model/tarefa.dart
+++ lib/model/tarefa.dart
@@ -20,4 +20,19 @@ class Tarefa{
     }
     return DateFormat('dd/MM/yyyy').format(prazo!);
   }
+
+  Map<String, dynamic> toMap() => <String, dynamic>{
+    CAMPO_ID : id,
+    CAMPO_DESCRICAO: descricao,
+    CAMPO_PRAZO: prazo == null ? null:
+    DateFormat("dd/MM/yyyy").format(prazo!)
+  };
+
+  factory Tarefa.fromMap(Map<String, dynamic> map) => Tarefa(
+      id: map[CAMPO_ID] is int ? map[CAMPO_ID] : null,
+      descricao: map[CAMPO_DESCRICAO] is String ? map[CAMPO_DESCRICAO] : '',
+      prazo: map[CAMPO_PRAZO] == null ? null :
+      DateFormat("dd/MM/yyyy").parse(map[CAMPO_PRAZO])
+  );
+
 }
