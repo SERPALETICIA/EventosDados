e6f8a1b997d562f7e6f854988b682a76f4aaa909
giovani <inf_giovani@hotmail.com>
2025-03-27 20:53:10 -0300

    dao completo

---
+++ lib/dao/tarefa_dao.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:gerenciador_tarefas_si7/database/database_provider.dart';
+
+import '../model/tarefa.dart';
+
+class TarefaDao{
+  final dbProvider = DatabaseProvider.instance;
+
+  Future<bool> salvar(Tarefa tarefa) async{
+    final db = await dbProvider.database;
+    final valores = tarefa.toMap();
+    if (tarefa.id == null){
+      tarefa.id = await db.insert(Tarefa.nomeTabela, valores);
+      return true;
+    }else{
+      final registrosAtualizados = await db.update(Tarefa.nomeTabela, valores,
+      where: '${Tarefa.CAMPO_ID} = ?', whereArgs: [tarefa.id]);
+      return registrosAtualizados > 0;
+    }
+  }
+
+  Future<bool> remover (int id) async{
+    final db = await dbProvider.database;
+    final registrosAtualizados = await db.delete(Tarefa.nomeTabela,
+    where:  '${Tarefa.CAMPO_ID} = ?', whereArgs: [id]);
+    return registrosAtualizados > 0;
+  }
+
+  Future<List<Tarefa>> listar() async{
+    final db = await dbProvider.database;
+    final resultado = await db.query(Tarefa.nomeTabela,
+        columns: [Tarefa.CAMPO_ID, Tarefa.CAMPO_DESCRICAO, Tarefa.CAMPO_PRAZO]);
+    return resultado.map((m) => Tarefa.fromMap(m)).toList();
+  }
+}
